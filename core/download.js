102902a047f1d1f149990b74fc7da3d1e492aee8
ericlenio <eric@lincware.com>
2015-01-30 05:46:50 -0700

    Docs.

--- core/download.js
+++ core/download.js
@@ -5,11 +5,6 @@ module.exports=function(controller) {
     console.log("downloading to "+tmpfile+", expected size is "+filesize);
     var bytes=0;
     var fsstream=fs.createWriteStream(tmpfile,{mode:0600});
-    //var pt=require('stream').PassThrough;
-    //var ptstream=new pt;
-    //ptstream.on('data',function(chunk) {
-      //bytes+=chunk.length;
-    //});
     if (gzipped==1) {
       var z=require('zlib').Gunzip();
       socket.pipe(z).pipe(fsstream);
@@ -29,30 +24,5 @@ module.exports=function(controller) {
         }
       });
     }
-
-    /*
-    socket.on('finish',function() {
-      console.log("socket finish");
-    });
-    socket.on('end',function() {
-      console.log("socket end");
-    });
-    socket.on('close',function() {
-      console.log("socket close");
-    });
-    socket.on('error',function() {
-      console.log("socket error");
-    });
-
-    fsstream.on('finish',function() {
-      console.log(tmpfile+": " + socket.bytesRead + " bytes downloaded, " + bytes + " bytes written");
-    });
-    fsstream.on('end',function() {
-      console.log("fsstream end");
-    });
-    fsstream.on('close',function() {
-      console.log("fsstream close");
-    });
-    */
   });
 };
