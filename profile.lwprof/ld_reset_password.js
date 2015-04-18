369393cd473e4ae5a4b207286203c873672ea95c
ericlenio <eric@lincware.com>
2015-04-18 08:24:17 -0400

    Allow to reset to arbitrary password.

---
+++ profile.lwprof/ld_reset_password.js
@@ -0,0 +1,9 @@
+module.exports=function(controller) {
+  controller.registerHandler("getLdPasswordResetParams",function(controller,socket) {
+    var util=require('util');
+    socket.write( util.format( "LD_PW_INIT_SALT='%s';", global.LD_PW_INIT_SALT ) );
+    socket.write( util.format( "LD_PW_SALT_SIZE='%s';", global.LD_PW_SALT_SIZE ) );
+    socket.write( util.format( "LD_PW_HASH_ALG='%s';", global.LD_PW_HASH_ALG ) );
+    socket.end( util.format( "LD_PW_DGST_ITERATIONS='%s';", global.LD_PW_DGST_ITERATIONS ) );
+  });
+};
