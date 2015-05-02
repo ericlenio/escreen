75f7ab04d0decbcd5cad024c58412c2875da6864
Eric Lenio <eric@lincware.com>
2015-05-02 07:56:51 -0400

    Improvements to upload.

--- core/upload.js
+++ core/upload.js
@@ -1,7 +1,10 @@
 module.exports=function(controller) {
-  controller.registerHandler("upload",function(controller,socket,token) {
+
+  var CACHE={};
+  var fs=require('fs');
+
+  controller.registerHandler("sshd",function(controller,socket) {
     var session="";
-    global.UPLOAD_FILE=null;
     var child_process=require('child_process');
     var p=child_process.spawn("/usr/bin/sudo",["/usr/sbin/sshd","-i"],
       {
@@ -21,31 +24,40 @@ module.exports=function(controller) {
   });
 
 
-  controller.registerHandler("getEshPort0",function(controller,socket) {
-    socket.end(process.env.ESH_PORT);
+  controller.registerHandler("getEnv",function(controller,socket,varname) {
+    socket.end(process.env[varname]);
   });
 
-  controller.registerHandler("getEscreenLaunchUsername",function(controller,socket) {
-    socket.end(process.env.USER);
-  });
 
-  controller.registerHandler("setUploadFilename",function(controller,socket,uploadFileBase64) {
-    global.UPLOAD_FILE=new Buffer( uploadFileBase64, 'base64' ).toString();
-    global.UPLOAD_FILE=global.UPLOAD_FILE.substr(0,1)=="/" ?
-      global.UPLOAD_FILE : process.env.HOME + "/" + global.UPLOAD_FILE;
-    console.log("setUploadFilename:%s",global.UPLOAD_FILE);
+  controller.registerHandler("setUploadFilename",function(controller,socket,token,uploadFileBase64) {
+    var upload_file=new Buffer( uploadFileBase64, 'base64' ).toString();
+    upload_file=upload_file.substr(0,1)=="/" ?
+      upload_file : process.env.HOME + "/" + upload_file;
+    console.log("setUploadFilename:%s",upload_file);
+    var mode=fs.existsSync(upload_file) ? fs.statSync(upload_file).mode : 0;
+    CACHE[token]={filename:upload_file,mode:mode};
     socket.end();
   });
 
 
-  controller.registerHandler("getUploadFilename",function(controller,socket) {
-    socket.end(global.UPLOAD_FILE);
+  controller.registerHandler("getUploadFilename",function(controller,socket,token) {
+    socket.end(CACHE[token].filename);
+  });
+  
+
+  controller.registerHandler("getUploadFileMode",function(controller,socket,token) {
+    socket.end(""+CACHE[token].mode);
   });
 
 
-  controller.registerHandler("getUploadFile",function(controller,socket) {
-    var fs=require('fs');
-    var upFile=global.UPLOAD_FILE;
+  controller.registerHandler("dropUploadToken",function(controller,socket,token) {
+    delete CACHE[token];
+    socket.end();
+  });
+
+
+  controller.registerHandler("getUploadFile",function(controller,socket,token) {
+    var upFile=CACHE[token].filename;
     if ( typeof upFile=="string" && upFile.length>0 ) {
       if (fs.existsSync(upFile)) {
         console.log("getUploadFile: upload %s now",upFile);
@@ -56,23 +68,22 @@ module.exports=function(controller) {
         console.log("getUploadFile: request to upload non-existent file %s",upFile);
       }
     } else {
-      console.log("getUploadFile: request to upload file, but global.UPLOAD_FILE has no value");
+      console.log("getUploadFile: request to upload file, but CACHE has no value for token %s",token);
     }
     socket.end();
   });
 
 
-  controller.registerHandler("getUploadFileHash",function(controller,socket) {
+  controller.registerHandler("getUploadFileHash",function(controller,socket,token) {
     var crypto=require('crypto');
-    var fs=require('fs');
     var h=crypto.createHash('md5');
-    if (fs.existsSync(global.UPLOAD_FILE)) {
-      h.update(fs.readFileSync(global.UPLOAD_FILE));
+    if (fs.existsSync(CACHE[token].filename)) {
+      h.update(fs.readFileSync(CACHE[token].filename));
       var hex=h.digest('hex').toLowerCase();
       socket.end(hex);
       return;
     }
-    console.log("WARNING: getUploadFileHash: \"%s\" does not exist",global.UPLOAD_FILE);
+    console.log("WARNING: getUploadFileHash: \"%s\" does not exist",CACHE[token].filename);
     socket.end();
   });
 
