be41f65d093729badb2e5e379d9f827157134bfa
Eric Lenio <eric@lincware.com>
2016-03-22 15:25:44 -0700

    Fix encoding.

--- profile.lwprof/get_lwsa_password.js
+++ profile.lwprof/get_lwsa_password.js
@@ -3,7 +3,7 @@ module.exports=function(controller) {
     var cp=require('child_process');
     var lwsauser="lwsa";
     // insert username/password
-    lwsa_url=lwsa_url.replace(/http:\/\/(.*)/,"http://"+lwsauser+":"+encodeURI(pw)+"@$1");
+    lwsa_url=lwsa_url.replace(/http:\/\/(.*)/,"http://"+lwsauser+":"+encodeURIComponent(pw)+"@$1");
     var open_prog=controller.getOsProgram("OPEN");
     var p3=cp.spawn(open_prog[0],[lwsa_url],{stdio:['ignore','ignore',null]});
     socket.end();
